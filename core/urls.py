bf6de36175993fbc30532270c97531f0896b6683
Duc Phuong <ducphuong200@gmail.com>
2021-05-26 23:10:17 +0700

    edit path checkout

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from .views import (HomeView,
                     ContactView,
                     ProductView,
                     CartView,
+                    CheckoutView,
                     LoginPage,
                     RegisterPage)
 
@@ -11,6 +12,7 @@ urlpatterns = [
     path('ContactUs/', ContactView.as_view(), name='contact'),
     path('Product/', ProductView.as_view(), name='product'),
     path('Cart/', CartView.as_view(), name='cart'),
+     path('Checkout/', CheckoutView.as_view(), name='checkout'),
     path('Login/', LoginPage.as_view(), name='login'),
     path('Register/', RegisterPage.as_view(), name='register')
 ]
